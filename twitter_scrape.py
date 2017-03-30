28637f6ba5aeeb46de53b9cf41b6e9b0ec33ffae
aotoole <aotoole@fwparker.org>
2017-03-30 10:57:46 -0500

    Final

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -5,3 +5,18 @@
 # Use BeautifulSoup and urllib to grab the text contents of the tweets
 # located on the twitter page you chose.  The .text attribute will supply the content of a soup object.
 # Have fun.  Again, nothing explicit. (15pts)
+
+from bs4 import BeautifulSoup
+import urllib.request
+
+url = 'https://twitter.com/martinmoran21?lang=en'
+
+page = urllib.request.urlopen(url)
+
+soup = BeautifulSoup(page.read(), 'html.parser')
+
+data_list = [x.text.strip() for x in soup.findAll('p', {'class' : 'js-tweet-text'})]
+
+for tweet in range(len(data_list)):
+    print(data_list[tweet])
+
--- weather_scrape.py
+++ weather_scrape.py
@@ -8,5 +8,32 @@
 # attributes to pull some parts of the data.
 # (e.g.  Wednesday, March 22: the high temp will be 48 with a low of 35, and a 20% chance of rain). (25pts)
 
+from bs4 import BeautifulSoup
+import urllib.request
 
 url = "https://weather.com/weather/tenday/l/USIL0225:1:US"
+
+page = urllib.request.urlopen(url)
+
+soup = BeautifulSoup(page.read(), 'html.parser')
+
+my_table= soup.find('table')
+print(my_table)
+headers = [x.text.strip() for x in my_table.findAll('th')]
+print(headers)
+
+data_list = [[y.text.strip() for y in x.findAll('td')] for x in soup.find('table').findAll('tr')][1:]
+
+data_list = [x[1:] for x in data_list]
+
+print(data_list)
+
+today = data_list[0][0][0:5]
+print(today)
+
+#degree_sign = u'\N{DEGREE SIGN]'
+
+print('The weather for', today +',', data_list[0][0][5:] , "is", data_list[0][1].lower()+ ". The high will be", data_list[0][2][0:2]+ "°F and the low will be", data_list[0][2][3:5] +'°F. The chance of rain is', data_list[0][3] +".")
+
+for pos in range(1, len(data_list)):
+    print('The weather for', data_list[pos][0][0:3] +',', data_list[pos][0][3:] , "is", data_list[pos][1].lower()+ ". The high will be", data_list[pos][2][0:2]+ "°F and the low will be", data_list[pos][2][3:5] +'°F. The chance of rain is', data_list[pos][3] +".")
